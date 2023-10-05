2400ee5d6db1671737a54981fe95df98998962ea
Coral <cnight18@hotmail.com>
2023-10-05 15:00:31 -0300

    trying to add framer transitions

---
+++ components/Transition.js
@@ -0,0 +1,32 @@
+// framer content here 
+import {motion} from 'framer-motion'
+
+
+// learning frameer tansitions , similar to key frames layout 
+const transtitionVariants ={
+  initial:{
+  x:'100%',
+  width:'100%'
+},
+animate:{
+  x:'0%'
+  width:'0%'
+},
+exit:{
+  x: ['0%','100%']
+  width:['0%','100%']
+},
+}
+
+const Transition = () => {
+  return (
+    <>
+      <div className=''</div>
+      <div>Transition</div>
+      <div>Transition</div>
+      <div>Transition</div>
+    </>
+  );
+};
+
+export default Transition;
--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,25 @@ import '../styles/globals.css';
 
 // add componets here 
 import Layout from '../components/Layout';
+import { Transition from } from '../components/Transition';
 
+// routes here
+import {useRouter} from 'next/router'
+
+//framer here
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
-  <Layout>
-  <Component {...pageProps} />;
-  </Layout>
+<Layout>
+<AnimatePresence mode='popLayout'>
+  <motion.div key={router.route} className='h-full'>
+    <Transition/> 
+     <Component {...pageProps} />;
+  </div>
+</AnimatePresence>
+</Layout>
   );
 }
 
