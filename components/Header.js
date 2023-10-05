7498725ad5af484d389de42fc8ecda0d0d25714f
Coral <cnight18@hotmail.com>
2023-10-05 12:04:01 -0300

    forgot name glow

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 text-7xl text-white z-50 p-4">
-      Coral Night
+      <span className="glow-animation">Coral Night</span> 
     </header>
   );
 };
