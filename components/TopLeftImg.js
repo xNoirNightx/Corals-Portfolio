193e2825fd59e68e681c5e0ee9f890eff76beabc
Coral <cnight18@hotmail.com>
2023-10-05 08:30:28 -0300

    tleft image set

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
+// top left image adjustments here , img increases size with screen size
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+  <div className='absolute  left-0 top-0 z-10 w-[400px] xl:w-[600px] opacity-50'>
+    <Image src='/tleft.png' width={700} height={700} alt=''/>
+  </div>
+  );
 };
 
 export default TopLeftImg;
