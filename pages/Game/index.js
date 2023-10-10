1f801d28e67e811c1ff4294ebf3cdf35a535df1a
Coral <cnight18@hotmail.com>
2023-10-10 13:55:23 -0300

    pending game section place holder

--- pages/Game/index.js
+++ pages/Game/index.js
@@ -1,6 +1,20 @@
+import React from "react";
+import { motion } from "framer-motion";
+
 const Game = () => {
-    return <div></div>;
-  };
-  
-  export default Game;
-  
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        exit={{ opacity: 0, y: -20 }} 
+        transition={{ duration: 0.5 }} 
+      >
+        <h2 className="text-7xl font-bold text-accent">Coming Soon...</h2>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Game;
+
