84481203172fe64ac9c5f311e5ebf647ac54dbdd
Coral <cnight18@hotmail.com>
2023-10-11 00:37:42 -0300

    heroku sucks

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,22 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 
 // components
-import Layout from '../components/Layout';
-import Transition from '../components/Transition';
+import Layout from "../components/Layout";
+import Transition from "../components/Transition";
 
 // router
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 // framer motion
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
-       <Component {...pageProps} />
-      <AnimatePresence mode='wait'>
-        <motion.div key={router.route} className='h-full'>
+      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
           <Transition />
         </motion.div>
       </AnimatePresence>
