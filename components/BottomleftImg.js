d6e2cbda585431152cce53b60c99acd5a74108e2
Coral <cnight18@hotmail.com>
2023-10-11 00:42:19 -0300

    still not working

--- components/BottomleftImg.js
+++ components/BottomleftImg.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const BottomLeftImg = () => {
   return (
     <div className='fixed bottom-0 left-0 z-10 w-[800px] xl:w-[800px] opacity-40'>
-      <Image src='/bleft.png' width={800} height={800} alt='' />
+      <Image src='/BottomLeftImg.png' width={800} height={800} alt='' />
     </div>
   );
 };
